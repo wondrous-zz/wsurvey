60931c25da53e1abd95fccccee736fdaffc6f316
root <tmohit736@gmail.com>
2013-07-29 03:18:57 -0700

    setting up the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wsurvey::Application.config.secret_key_base = 'abd76110a26ee7450cc03afe73f579978197552f656b189fb0e19bc69c477efce48fc70015387835f0cffec5f5b9b2b959f46f3e10ec703b566b977a235828ba'
